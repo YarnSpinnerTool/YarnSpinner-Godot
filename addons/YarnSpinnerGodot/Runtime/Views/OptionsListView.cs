e3fbfcc2cf5384a0346ac2394a5dacb0144c03e1
dogboydog <dogboydog@users.noreply.github.com>
2022-12-09 20:09:35 -0500

    progressing on OptionsListView.cs integration (options not working yet)

---
+++ addons/YarnSpinnerGodot/Runtime/Views/OptionsListView.cs
@@ -0,0 +1,154 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Godot;
+
+namespace Yarn.GodotIntegration
+{
+    public partial class OptionsListView : DialogueViewBase
+    {
+        [Export] PackedScene optionViewPrefab;
+
+        [Export] private NodePath lastLineTextPath;
+        RichTextLabel lastLineText;
+
+        /// <summary>
+        /// The Control that is the parent of all UI elements in this line view.
+        /// Used to modify the transparency/visibility of the UI.
+        /// </summary>
+        [Export] private NodePath viewControlPath;
+        private Control viewControl;
+         
+        [Export] float fadeTime = 0.1f;
+
+        [Export] bool showUnavailableOptions = false;
+
+        // A cached pool of OptionView objects so that we can reuse them
+        List<OptionView> optionViews = new List<OptionView>();
+
+        // The method we should call when an option has been selected.
+        Action<int> OnOptionSelected;
+
+        // The line we saw most recently.
+        LocalizedLine lastSeenLine;
+
+        public override void _Ready()
+        {
+            if (viewControl == null)
+            {
+                viewControl = GetNode<Control>(viewControlPath);
+            }
+            viewControl.Visible = false;
+        }
+
+        public override void RunLine(LocalizedLine dialogueLine, Action onDialogueLineFinished)
+        {
+            // Don't do anything with this line except note it and
+            // immediately indicate that we're finished with it. RunOptions
+            // will use it to display the text of the previous line.
+            lastSeenLine = dialogueLine;
+            onDialogueLineFinished();
+        }
+
+        public override void RunOptions(DialogueOption[] dialogueOptions, Action<int> onOptionSelected)
+        {
+            // Hide all existing option views
+            foreach (var optionView in optionViews)
+            {
+                optionView.Visible = false;
+            }
+
+            // If we don't already have enough option views, create more
+            while (dialogueOptions.Length > optionViews.Count)
+            {
+                var optionView = CreateNewOptionView();
+                optionView.Visible = false;
+            }
+
+            // Set up all of the option views
+            int optionViewsCreated = 0;
+
+            for (int i = 0; i < dialogueOptions.Length; i++)
+            {
+                var optionView = optionViews[i];
+                var option = dialogueOptions[i];
+
+                if (option.IsAvailable == false && showUnavailableOptions == false)
+                {
+                    // Don't show this option.
+                    continue;
+                }
+
+                optionView.Visible = true;
+
+                optionView.Option = option;
+
+                // The first available option is selected by default
+                if (optionViewsCreated == 0)
+                {
+                    optionView.GrabFocus();
+                }
+
+                optionViewsCreated += 1;
+            }
+
+            // Update the last line, if one is configured
+            if (lastLineText != null)
+            {
+                if (lastSeenLine != null)
+                {
+                    lastLineText.Visible = true;
+                    lastLineText.Text = lastSeenLine.Text.Text;
+                }
+                else
+                {
+                    lastLineText.Visible = false;
+                }
+            }
+
+            // Note the delegate to call when an option is selected
+            OnOptionSelected = onOptionSelected;
+
+            // Fade it all in
+            Effects.FadeAlpha(viewControl, 0, 1, fadeTime)
+                .ContinueWith(t =>
+                {
+                    if (t.IsFaulted)
+                    {
+                        GD.PrintErr($"Error running {nameof(Effects.FadeAlpha)} on {nameof(OptionsListView)}: {t.Exception}");
+                    }
+                });
+                    
+            /// <summary>
+            /// Creates and configures a new <see cref="OptionView"/>, and adds
+            /// it to <see cref="optionViews"/>.
+            /// </summary>
+            OptionView CreateNewOptionView()
+            {
+                var optionView = optionViewPrefab.Instantiate<OptionView>();
+                viewControl.AddChild(optionView);
+
+                optionView.OnOptionSelected = OptionViewWasSelected;
+                optionViews.Add(optionView);
+
+                return optionView;
+            }
+
+            /// <summary>
+            /// Called by <see cref="OptionView"/> objects.
+            /// </summary>
+            void OptionViewWasSelected(DialogueOption option)
+            {
+                OptionViewWasSelectedInternal(option).ContinueWith(t =>
+                    GD.PrintErr($"Error running {nameof(OptionViewWasSelected)} on {nameof(OptionsListView)}: {t.Exception}"));
+
+                async Task OptionViewWasSelectedInternal(DialogueOption selectedOption)
+                {
+                    await Effects.FadeAlpha(viewControl, 1, 0, fadeTime);
+                    OnOptionSelected(selectedOption.DialogueOptionID);
+                }
+            }
+        }
+    }
+}
