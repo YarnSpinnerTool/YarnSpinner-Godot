69399d22f6a8228088da0b3b13dc3762a0e5fb24
dogboydog <dogboydog@users.noreply.github.com>
2022-12-10 20:47:15 -0500

    add listener to onDialogueComplete

---
+++ Samples/Space/Scripts/SpaceSample.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+using Yarn.GodotIntegration;
+
+public class SpaceSample : Node
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    [Export] public NodePath dialogueRunnerPath;
+
+    private DialogueRunner _dialogueRunner;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _dialogueRunner = GetNode<DialogueRunner>(dialogueRunnerPath);
+        _dialogueRunner.onDialogueComplete += OnDialogueComplete;
+    }
+
+    private void OnDialogueComplete()
+    {
+        GD.Print("Space sample has completed!");
+    }
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
