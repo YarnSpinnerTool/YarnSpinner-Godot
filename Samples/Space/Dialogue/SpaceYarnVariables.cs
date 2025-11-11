47e33b4db0b0a5cc28b6c09ed59df01bed8ac927
dogboydog <dogboydog@users.noreply.github.com>
2025-11-11 17:27:40 -0500

    regenerated variable storage

--- Samples/Space/Dialogue/SpaceYarnVariables.cs
+++ Samples/Space/Dialogue/SpaceYarnVariables.cs
@@ -2,24 +2,6 @@ using YarnSpinnerGodot;
 
 [System.CodeDom.Compiler.GeneratedCode("YarnSpinner", "0.3.0")]
 public partial class SpaceYarnVariables : YarnSpinnerGodot.InMemoryVariableStorage, YarnSpinnerGodot.IGeneratedVariableStorage {
-    // Accessor for Bool $should_see_ship
-    /// <summary>
-    /// If the player has been told they should talk to the ship.
-    /// </summary>
-    public bool ShouldSeeShip {
-        get => this.GetValueOrDefault<bool>("$should_see_ship");
-        set => this.SetValue<bool>("$should_see_ship", value);
-    }
-
-    // Accessor for Bool $sally_warning
-    /// <summary>
-    /// Whether the ship has relayed Sally's warning to the player.
-    /// </summary>
-    public bool SallyWarning {
-        get => this.GetValueOrDefault<bool>("$sally_warning");
-        set => this.SetValue<bool>("$sally_warning", value);
-    }
-
     // Accessor for String $sampleName
     public string SampleName {
         get => this.GetValueOrDefault<string>("$sampleName");
@@ -41,6 +23,24 @@ public partial class SpaceYarnVariables : YarnSpinnerGodot.InMemoryVariableStora
     public bool ApologizedToSally {
         get => this.GetValueOrDefault<bool>("$apologized_to_sally");
         set => this.SetValue<bool>("$apologized_to_sally", value);
+    }
+
+    // Accessor for Bool $should_see_ship
+    /// <summary>
+    /// If the player has been told they should talk to the ship.
+    /// </summary>
+    public bool ShouldSeeShip {
+        get => this.GetValueOrDefault<bool>("$should_see_ship");
+        set => this.SetValue<bool>("$should_see_ship", value);
+    }
+
+    // Accessor for Bool $sally_warning
+    /// <summary>
+    /// Whether the ship has relayed Sally's warning to the player.
+    /// </summary>
+    public bool SallyWarning {
+        get => this.GetValueOrDefault<bool>("$sally_warning");
+        set => this.SetValue<bool>("$sally_warning", value);
     }
 
 }
