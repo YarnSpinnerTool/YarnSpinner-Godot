26e5b0caee8828523d7a72ef946f89ef48afa648
dogboydog <6483507-dogboydog@users.noreply.gitlab.com>
2026-03-27 11:34:21 -0400

    WIP update to yarnspinner 3.2.0 libs

--- addons/YarnSpinner-Godot/Runtime/Async/PaletteMarkerProcessor.cs
+++ addons/YarnSpinner-Godot/Runtime/Async/PaletteMarkerProcessor.cs
@@ -39,14 +39,17 @@ public partial class PaletteMarkerProcessor : ReplacementMarkupHandler
     /// apply, but this is ignored for TextMeshPro styles.</param>
     /// <param name="localeCode">The locale code to use when formatting the style.</param>
     /// <returns>A list of markup diagnostics if there are any errors, otherwise an empty list.</returns>
-    public override List<LineParser.MarkupDiagnostic> ProcessReplacementMarker(MarkupAttribute marker,
+    public override ReplacementMarkerResult ProcessReplacementMarker(MarkupAttribute marker,
         StringBuilder childBuilder, List<MarkupAttribute> childAttributes, string localeCode)
     {
         if (palette == null)
         {
-            var list = new List<LineParser.MarkupDiagnostic>
-                { new($"No palette set on {nameof(PaletteMarkerProcessor)}") };
-            return list;
+            var error = new List<LineParser.MarkupDiagnostic>
+            {
+                new LineParser.MarkupDiagnostic(
+                    $"can't apply palette for marker {marker.Name}, because a palette was not set")
+            };
+            return new ReplacementMarkerResult(error, 0);
         }
 
 
@@ -70,11 +73,19 @@ public partial class PaletteMarkerProcessor : ReplacementMarkupHandler
                 }
             }
 
-            return ReplacementMarkupHandler.NoDiagnostics;
+            // finally we need to calculate the number of invisible characters we added
+            // which is the difference between the new and original string lengths - the total number of visible characters inserted
+            // we don't care WHERE those visible characters were added, just that they were
+            // we can't just use the marker offset because that only worries about visible elements added at the front of the string
+            // most of the time this is just gonna be 0 anyways and you don't have to think about it
+            return new ReplacementMarkerResult(childBuilder.Length - childrenLength -
+                                               format.TotalVisibleCharacterCount);
         }
 
-        var diagnostic = new LineParser.MarkupDiagnostic($"was unable to find a matching marker for {marker.Name}");
-        return new List<LineParser.MarkupDiagnostic>() { diagnostic };
+        List<LineParser.MarkupDiagnostic> diagnostics =
+                  [new($"was unable to find a matching marker for {marker.Name}")];
+
+        return new ReplacementMarkerResult(diagnostics, 0);
     }
 
     /// <summary>
